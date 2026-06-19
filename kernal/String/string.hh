9b11b19584ffe5214f90ba26cb3f84bffd224dd3
Adharsh Shokkalingam <harsh.shokka@gmail.com>
2026-06-19 15:59:19 -0600

    update

--- kernal/String/string.hh
+++ kernal/String/string.hh
@@ -5,7 +5,20 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+int memcmp(const void* , const void *, size_t); // memeory compare
+void* memcpy(void* __restrict, const void* __restrict, size_t);
+void memmove(void*, const void*, size_t);
+void* memset(void*, int, size_t);
+
 
 size_t strlen(const char *str);
 
+#ifdef __cplusplus
+}
+#endif
 #endif 
