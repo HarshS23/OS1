58bef3144b6e91aafa8ecb179e1a5f1c71f810e4
Adharsh Shokkalingam <harsh.shokka@gmail.com>
2026-06-26 11:21:18 -0600

    prep for exceptions

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -17,8 +17,23 @@ void kernel_main(void) {
 
 	// testing if the interrupt works 
 	//__asm__ volatile ("int $0x3");   
+	printf("####################################################\n");
+	printf("                 Hello, kernel World!               \n");
+	printf("####################################################\n");
+
+	printf("\nThis is my Operating System Project. I made this to gain a better understanding"
+		    " of how a computer works more specifically an Operating System.\n"
+		    "I've currently finished the Global Discriptor Table, and a basic Interrupt \n Descriptor Table."
+			" My future plans are to work on\n"
+			"\n1. Exceptions \n" 
+			"2. IRQ \n" 
+			"3. System Calls \n"
+			"4. Paging (Basic Demand Paging),\n"
+			"5. Memeory Management \n");
 	
-	printf("\nHello, kernel World!\n");
+	printf("####################################################\n");
+	
+
 
 
 	__asm__ volatile ("int $0x3");   
