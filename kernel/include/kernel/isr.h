434951de4101efccb7742a2ff9f3d2068a4458c5
Adharsh Shokkalingam <harsh.shokka@gmail.com>
2026-06-29 15:08:37 -0600

    working on Interupt Service Routine

---
+++ kernel/include/kernel/isr.h
@@ -0,0 +1,26 @@
+#ifndef _ISR_H_
+#define _ISR_H_
+
+#include <stdint.h>
+#include <stdio.h>
+
+#define ENABLE_INTERRUPTS \
+    __asm__ __volatile__("sti":::)
+
+#define PAUSE_CPU \
+    __asm__ __volatile__("hlt":::)
+
+
+typedef struct {
+    uint32_t VectorNum;
+    uint32_t ErrorCode;
+    uint32_t eip; // Instruction Pointer 
+    uint32_t cs; // code segment 
+    uint32_t eflags; // flages register
+    uint32_t esp; // stack pointer 
+    uint32_t ss; // stack segment 
+
+}int_frame_t; // frame in bytes
+
+
+#endif
